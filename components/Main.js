f544a9daad384555025ab5a43417b48eb0dcc2b5
Paul Yun <paul.a.yun@gmail.com>
2020-11-20 13:41:35 -0800

    Add custom drawer

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,9 @@
 import React, { Component, useState } from 'react'
-import { Platform, View } from 'react-native'
+import { Platform, View, StyleSheet, Image, Text, ScrollView } from 'react-native'
+import { Icon } from 'react-native-elements'
+import SafeAreaView from 'react-native-safe-area-view'
 import { createAppContainer } from 'react-navigation'
-import { createDrawerNavigator } from 'react-navigation-drawer'
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer'
 import { createStackNavigator } from 'react-navigation-stack'
 
 import { CampsiteInfo } from './CampsiteInfo.js'
@@ -10,7 +12,17 @@ import { Home } from './Home.js'
 
 const DirectorNavigator = createStackNavigator(
   {
-    Directory: { screen: Directory },
+    Directory: {
+      screen: Directory,
+      navigationOptions: ({ navigation }) => ({
+        headerLeft: () => <Icon
+          name='list'
+          type='font-awesome'
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      })
+     },
     CampsiteInfo: { screen: CampsiteInfo },
   },
   {
@@ -32,7 +44,7 @@ const HomeNavigator = createStackNavigator(
     Home: { screen: Home }
   },
   {
-    defaultNavigationOptions: {
+    defaultNavigationOptions: ({ navigation }) => ({
       headerStyle: {
         backgroundColor: '#5637DD',
       },
@@ -40,21 +52,103 @@ const HomeNavigator = createStackNavigator(
       headerTitleStyle: {
         color: '#fff',
       },
-    }
+      headerLeft: () => <Icon
+          name='home'
+          type='font-awesome'
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+    })
   },
+)
+
+const CustomDrawerContent = props => (
+  <ScrollView>
+    <SafeAreaView
+      style={styles.container}
+      forceInset={{ top: 'always', horizontal: 'never' }}
+    >
+      <View style={styles.drawerHeader}>
+        <View style={{ flex: 1 }}>
+          <Image
+            source={require('../assets/images/logo.png')}
+            style={styles.drawerImage}
+          />
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text style={styles.drawerHeaderText}>NuCamp</Text>
+        </View>
+      </View>
+      <DrawerItems {...props} />
+    </SafeAreaView>
+  </ScrollView>
 )
 
 const MainNavigator = createDrawerNavigator(
   {
-    Home: { screen: HomeNavigator },
-    Directory: { screen: DirectorNavigator },
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon
+            name='home'
+            type='font-awesome'
+            size={24}
+            color={tintColor}
+          />
+        )
+      }
+    },
+    Directory: {
+      screen: DirectorNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon
+            name='list'
+            type='font-awesome'
+            size={24}
+            color={tintColor}
+          />
+        )
+      }
+    },
   },
   {
     drawerBackgroundColor: '#cec8ff',
+    contentComponent: CustomDrawerContent,
   }
 )
 
 const AppNavigator = createAppContainer(MainNavigator)
+
+const styles = StyleSheet.create({
+  stackIcon: {
+    marginLeft: 10,
+    color: '#fff',
+    fontSize: 24,
+  },
+  container: {
+    flex: 1,
+  },
+  drawerHeader: {
+    backgroundColor: '#5637DD',
+    height: 140,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+  },
+  drawerHeaderText: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  drawerImage: {
+    margin: 10,
+    height: 60,
+    width: 60,
+  },
+})
 
 export const Main = () => {
   return (
