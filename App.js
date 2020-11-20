9214bc3bfd0435760b9cb25b39d4a372393be018
Paul Yun <paul.a.yun@gmail.com>
2020-11-20 13:03:53 -0800

    Add favorite features, fix FlatList warning

--- App.js
+++ App.js
@@ -1,29 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 import { Main } from './components/Main'
 
 // to hide warnings uncomment below
-// console.disableYellowBox = true;
+console.disableYellowBox = true
 
 const App = () => {
   return (
     <Main />
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#a9a9a9',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 50,
-    textAlign: 'center',
-  },
-});
 
 export default App
--- components/CampsiteInfo.js
+++ components/CampsiteInfo.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Text, View, ScrollView, FlatList } from 'react-native'
-import { Card } from 'react-native-elements'
+import { Card, Icon } from 'react-native-elements'
 
 import { CAMPSITES } from '../shared/campsites'
 import { COMMENTS } from '../shared/comments'
 
-const Campsite = ({ campsite }) =>
+const Campsite = ({ campsite, isFavorite, toggleFavorite }) =>
   <>
     {!!campsite ? (
       <Card
@@ -15,13 +15,21 @@ const Campsite = ({ campsite }) =>
         <Text style={{ margin: 10 }}>
           {campsite.description}
         </Text>
+        <Icon
+          name={isFavorite ? 'heart' : 'heart-o'}
+          type='font-awesome'
+          color='#f50'
+          raised
+          reverse
+          onPress={toggleFavorite}
+        />
       </Card>
     ) : (
       <View />
     )}
   </>
 
-const Comment = ({ item: comment }) =>
+const Comment = ({ comment }) =>
   <View style={{ margin: 10 }}>
     <Text style={{ fontSize: 14 }}>{comment.text}</Text>
     <Text style={{ fontSize: 12 }}>{comment.rating} Stars</Text>
@@ -30,17 +38,22 @@ const Comment = ({ item: comment }) =>
 
 const Comments = ({ comments }) =>
   <Card title='Comments'>
-    <FlatList
-      data={comments}
-      renderItem={Comment}
-      keyExtractor={item => item.id.toString()}
-    />
+    {comments.map(c => <Comment comment={c} key={c.id} />)}
   </Card>
 
 const getCampsite = campsiteId => CAMPSITES.find(c => c.id === campsiteId)
 const getComments = campsiteId => COMMENTS.filter(c => c.campsiteId === campsiteId)
 
 export class CampsiteInfo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isFavorite: false
+    }
+  }
+
+  toggleFavorite = () => this.setState({ isFavorite: !this.state.isFavorite })
+
   static navigationOptions = { title: 'Campsite Information' }
 
   render() {
@@ -48,7 +61,11 @@ export class CampsiteInfo extends React.Component {
 
     return (
       <ScrollView>
-        <Campsite campsite={getCampsite(campsiteId)} />
+        <Campsite
+          campsite={getCampsite(campsiteId)}
+          isFavorite={this.state.isFavorite}
+          toggleFavorite={this.toggleFavorite}
+        />
         <Comments comments={getComments(campsiteId)} />
       </ScrollView>
     )
