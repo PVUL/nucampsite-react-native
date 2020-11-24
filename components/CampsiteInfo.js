e8b1016cb1da7b23b8b32c98eb66926957d8a349
Paul Yun <paul.a.yun@gmail.com>
2020-11-24 11:27:40 -0800

    Add animations to directory and campsite info

--- components/CampsiteInfo.js
+++ components/CampsiteInfo.js
@@ -1,29 +1,35 @@
 import React, { useState } from 'react'
 import { Text, View, ScrollView } from 'react-native'
 import { Card, Icon } from 'react-native-elements'
+import * as Animatable from 'react-native-animatable'
 
 import { CAMPSITES } from '../shared/campsites'
 import { COMMENTS } from '../shared/comments'
 
+ANIMATION_DELAY = 500
+ANIMATION_DURATION = 1000
+
 const Campsite = ({ campsite, isFavorite, toggleFavorite }) =>
   <>
     {!!campsite ? (
-      <Card
-        featuredTitle={campsite.name}
-        image={require('../assets/images/react-lake.jpg')}
-      >
-        <Text style={{ margin: 10 }}>
-          {campsite.description}
-        </Text>
-        <Icon
-          name={isFavorite ? 'heart' : 'heart-o'}
-          type='font-awesome'
-          color='#f50'
-          raised
-          reverse
-          onPress={toggleFavorite}
-        />
-      </Card>
+      <Animatable.View animation='fadeInDown' duration={ANIMATION_DURATION} delay={ANIMATION_DELAY}>
+        <Card
+          featuredTitle={campsite.name}
+          image={require('../assets/images/react-lake.jpg')}
+        >
+          <Text style={{ margin: 10 }}>
+            {campsite.description}
+          </Text>
+          <Icon
+            name={isFavorite ? 'heart' : 'heart-o'}
+            type='font-awesome'
+            color='#f50'
+            raised
+            reverse
+            onPress={toggleFavorite}
+          />
+        </Card>
+      </Animatable.View>
     ) : (
       <View />
     )}
@@ -37,9 +43,11 @@ const Comment = ({ comment }) =>
   </View>
 
 const Comments = ({ comments }) =>
-  <Card title='Comments'>
-    {comments.map(c => <Comment comment={c} key={c.id} />)}
-  </Card>
+  <Animatable.View animation='fadeInUp' duration={ANIMATION_DURATION} delay={ANIMATION_DELAY}>
+    <Card title='Comments'>
+      {comments.map(c => <Comment comment={c} key={c.id} />)}
+    </Card>
+  </Animatable.View>
 
 const getCampsite = campsiteId => CAMPSITES.find(c => c.id === campsiteId)
 const getComments = campsiteId => COMMENTS.filter(c => c.campsiteId === campsiteId)
--- components/Directory.js
+++ components/Directory.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { FlatList } from 'react-native'
-import { ListItem } from 'react-native-elements'
+import { Tile } from 'react-native-elements'
+import * as Animatable from 'react-native-animatable'
 
 import { CAMPSITES } from '../shared/campsites'
 
 const DirectoryComponent = ({ navigation: { navigate } }) => {
   const renderDirectoryItem = ({ item }) =>
-    <ListItem
-      title={item.name}
-      subtitle={item.description}
-      leftAvatar={{ source: require('../assets/images/react-lake.jpg') }}
-      onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-    />
+    <Animatable.View animation='fadeInRightBig' duration={2000}>
+      <Tile
+        title={item.name}
+        caption={item.description}
+        imageSrc={require('../assets/images/react-lake.jpg')}
+        onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+        featured
+      />
+    </Animatable.View>
 
   return (
     <FlatList
